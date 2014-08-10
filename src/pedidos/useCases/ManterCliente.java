243ef8a5168359d73acba7b933cfcf87add1a169
Guepardo <bsinet@hotmail.com>
2014-08-10 03:09:05 -0300

    Mais algumas classes.

---
+++ src/pedidos/useCases/ManterCliente.java
@@ -0,0 +1,55 @@
+package pedidos.useCases;
+
+import pedidos.control.CrudController;
+import pedidos.control.ModelController;
+import pedidos.model.Cliente;
+import pedidos.util.ActionDone;
+import pedidos.util.DoAction;
+
+public class ManterCliente extends ModelController {
+	private CrudController cc;
+	
+	public ManterCliente(){
+		this.cc = new CrudController();
+	};
+	
+	public ActionDone incluir( DoAction da){
+		Cliente c = new Cliente();
+		c.biuldObject(da);
+		ActionDone ad = cc.save(c.getColumnName(),c.getColumnName(),c.getColumnValues());
+		
+		ad.setAction(da.getAction());
+		ad.setUserCase(da.getUserCase());
+		ad.setStatus(true);
+		ad.setMessage("Você foi cadastrado com sucesso.");
+		return ad;
+	};
+	
+	public ActionDone alterar( DoAction ad){
+		return null;
+	};
+	
+	public ActionDone excluir( DoAction ad){
+		return null;
+	};
+	
+	public ActionDone consultar( DoAction ad){
+		return null;
+	};
+	
+	@Override
+	public String[] getActions() {
+		String [] temp ={
+							"incluir",
+							"alterar",
+							"excluir",
+							"consultar"
+						};
+		return temp;
+	};
+
+	@Override
+	public String getUserCase() {
+		return "manterCliente";
+	};
+}
---
+++ src/pedidos/viewUseCases/ManterClienteView.java
@@ -0,0 +1,13 @@
+package pedidos.viewUseCases;
+
+import pedidos.util.ActionDone;
+import pedidos.view.ViewController;
+
+public class ManterClienteView extends ViewController {
+
+	@Override
+	public String choose(ActionDone ad) {
+		return null;
+	}
+
+}
