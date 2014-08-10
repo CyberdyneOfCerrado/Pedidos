bcbe159845466d89867145e59f904893bf55e072
Guepardo <bsinet@hotmail.com>
2014-08-10 03:30:27 -0300

    3:30am

--- src/pedidos/view/ViewController.java
+++ src/pedidos/view/ViewController.java
@@ -1,7 +1,33 @@
 package pedidos.view;
 
+import java.io.IOException;
+
+import biz.source_code.miniTemplator.MiniTemplator;
+import biz.source_code.miniTemplator.MiniTemplator.TemplateSyntaxException;
 import pedidos.util.ActionDone;
 
 public abstract class ViewController {
+	private String sevletContext;
+	private String userCase;
+	private String separador;
+	
+	public ViewController(String sevletContext,String userCase ){
+		separador = System.getProperty("file.separator");
+		this.sevletContext = sevletContext;
+		this.userCase = userCase+separador;
+	};
+	
+	private MiniTemplator startMiniTemplator(String path){
+		MiniTemplator mi = null;
+		try {
+			System.out.println(path);
+			mi = new MiniTemplator(path);
+		} catch (TemplateSyntaxException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return mi;
+	};
+	
 	public abstract String choose( ActionDone ad );
 }
--- src/pedidos/viewUseCases/ManterClienteView.java
+++ src/pedidos/viewUseCases/ManterClienteView.java
@@ -5,8 +5,22 @@ import pedidos.view.ViewController;
 
 public class ManterClienteView extends ViewController {
 
+	public ManterClienteView(String sevletContext, String userCase) {
+		super(sevletContext, userCase);
+	}
+	
+	private String incluir ( ActionDone ad){
+		
+		return null;
+	}
+
 	@Override
 	public String choose(ActionDone ad) {
+		String resul;
+		switch(ad.getAction()){
+		case "incluir":
+			break;
+		}
 		return null;
 	}
 
